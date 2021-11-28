78dc14b9895d6d440cc6092c0b65138541a66d76
Djebouh <djebouh@yahoo.fr>
2021-11-28 18:47:21 +0100

    on 2021.11.5, after many months...

--- scripts/zwave-js-server.sh
+++ scripts/zwave-js-server.sh
@@ -2,3 +2,4 @@
 
 cd /home/homeassistant/zwave-js-server/
 ts-node "src/bin/server.ts" --config "/home/homeassistant/zwave-js-server/key.json" /dev/zwaveusbstick
+# ts-node "src/bin/server.ts" /dev/zwaveusbstick
